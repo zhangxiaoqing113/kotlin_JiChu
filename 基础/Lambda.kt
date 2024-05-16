eb446aaa42aa1364ae2a6e188ad7c5365bd418fd
ZhangXiaoQing <zhangxiaoqing113@163.com>
2024-05-16 11:14:52 +0800

    添加新的感悟

--- 基础/Lambda.kt
+++ 基础/Lambda.kt
@@ -2,7 +2,7 @@
  * Lambda 表达式 定义: var 变量名={参数名:参数类型,参数名:参数类型->返回值}
  *                   var sum={x:Int,y:Int->Int}
  *
- * 当lambda表达式作为 方法的形参时写为  变量名:(参数名:参数类型,参数名:参数类型)->返回值类型
+ * 当lambda表达式作为 方法的形参时 写为  变量名:(参数名:参数类型,参数名:参数类型)->返回值类型. 如下边的block
  * fun test(x: Int, y: Int, block: (a: Int, b: Int) -> Int): Unit {}
  */
 
@@ -19,11 +19,19 @@
  *     return a+b;
  *    }
  *
- *
- *  传参这里做一个参数说明 {x,y->x+y}  其实里边叫 x y m n b d g c v任何变量名都行
+ *   {x,y->x+y} 把这个lambda拆分为两部分 (1) 参数声明 x,y  (2) 方法体 ->x+y
+ *   (1) x,y
+ *  其实里边叫 x y m n b d g c v任何变量名都行
  *  只是对形参 block: (a: Int, b: Int) a,b换了个名字叫而已,
- *  最重要的是后边的函数体 x+y
+ *  ⭐⭐⭐但是必须要在lambda表达式中声明出来
  *
+ *  (2) ->x+y 可以看成方法体 跟 普通函数的方法体 一样
+ *
+ *   ⭐fun ppp(a: Int, b: Int) -> Int{
+ *    ⭐ 方法体 跟普通的函数一样
+ *     ⭐
+ *     return a+b;
+ *    }
  *
  */
 fun testppp(x: Int, y: Int, block: (a: Int, b: Int) -> Int) {
